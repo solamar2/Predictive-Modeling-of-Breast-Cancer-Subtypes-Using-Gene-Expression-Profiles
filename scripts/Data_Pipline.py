d4829fb5e5065fcc24e139ccadf11897240e3513
solamar2 <solamar@post.bgu.ac.il>
2025-12-09 12:35:59 +0200

    Update - data manager

--- scripts/Data_Pipline.py
+++ scripts/Data_Pipline.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from scripts.Data_manager.Data_Loading import DataLoading
 from scripts.Data_manager.Data_Preprocessing import Preprocessing
 from scripts.Data_manager.Data_Split import DataSplit
@@ -41,33 +40,6 @@ class Pipline:
 if __name__ == "__main__":
     pipeline = Pipline()
     pipeline.main()
-=======
-import scripts.Data_manager as DM
-
-class Pipline():
-    
-    
-    def main():
-        expr_path="data/raw/data_mrna_illumina_microarray_zscores_ref_diploid_samples.txt",
-        clinical_path="data/raw/clinical_patient_data.txt",
-        relevant_subtypes = ['LumA', 'LumB', 'Her2', 'Basal', 'Normal']
-        
-        # load data:
-        GenesData, labels= DM.load_data(expr_path, clinical_path, relevant_subtypes)
-        
-        
-        # Pre-process:
-        pre = DM.Preprocessing(zscore=True, pca_components=200)
-        GenesData_proc, labels_proc = pre.fit_transform(GenesData, labels)
-
-#X_test_proc, y_test_proc = pre.transform(X_test, y_test)
-
-   if __name__ == "__main__":
-    main() 
-    
-
->>>>>>> 58782fb9c1e65a678d3c1f4d6d6498eefe511b78
-
 
 
 
--- scripts/Data_manager/Data_Preprocessing.py
+++ scripts/Data_manager/Data_Preprocessing.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
@@ -113,33 +112,4 @@ class Preprocessing:
 
         return (X_proc, y_proc) if y is not None else X_proc
 
-   
-
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  7 15:49:48 2025
-
-@author: solam
-"""
-
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-from keras.utils import to_categorical
-
-class Preprocessor:
-    def __init__(self):
-        self.scaler = None
-        self.label_encoder = None
-
-    def scale_X(self, X):
-        self.scaler = StandardScaler()
-        return self.scaler.fit_transform(X)
-
-    def encode_y(self, y, onehot=True):
-        self.label_encoder = LabelEncoder()
-        y_int = self.label_encoder.fit_transform(y)
-        if onehot:
-            y_encoded = to_categorical(y_int)
-            return y_encoded
-        return y_int
->>>>>>> 58782fb9c1e65a678d3c1f4d6d6498eefe511b78
+  
